ee1da81369f8173c21a010b6fe3530594b200355
xfja <franjimenezaguilera@gmail.com>
2020-05-28 21:46:27 +0200

    Add enum for sensor PID

--- src/models/dataset.ts
+++ src/models/dataset.ts
@@ -10,11 +10,39 @@ export interface Log {
   records: Record[];
 }
 
-// TODO: create enum for sensorPID
 export interface Record {
   id: number;
   value: number;
-  sensorPID: string;
+  sensorPID: SensorPID;
+}
+
+enum SensorPID {
+  EngineRPM = "ENGINE_RPM",
+  VehicleSpeed = "VEHICLE_SPEED",
+  Throttle = "THROTTLE",
+  EngineLoad = "ENGINE_LOAD",
+  CoolantTemperature = "COOLANT_TEMPERATURE",
+  LongTermFuelTrimBank1 = "LONG_TERM_FUEL_TRIM_BANK_1",
+  ShortTermFuelTrimBank1 = "SHORT_TERM_FUEL_TRIM_BANK_1",
+  IntakeManifoldPressure = "SHORT_TERM_FUEL_TRIM_BANK_1",
+  FuelTank = "FUEL_TANK",
+  AbsoluteThrottleB = "ABSOLUTE_THROTTLE_B",
+  PedalD = "PEDAL_D",
+  PedalE = "PEDAL_E",
+  CommandThrottleActuator = "COMMAND_THROTTLE_ACTUATOR",
+  FuelAirCommandedEquivRatio = "FUEL_AIR_COMMANDED_EQUIV_RATIO",
+  AbsoluteBarometricPressure = "ABSOLUTE_BAROMETRIC_PRESSURE",
+  RelativeThrottlePosition = "RELATIVE_THROTTLE_POSITION",
+  IntakeAirTemp = "INTAKE_AIR_TEMP",
+  TimingAdvance = "TIMING_ADVANCE",
+  CatalystTemperatureBank1Sensor1 = "CATALYST_TEMPERATURE_BANK1_SENSOR1",
+  CatalystTemperatureBank1Sensor2 = "CATALYST_TEMPERATURE_BANK1_SENSOR2",
+  ControlModuleVoltage = "CONTROL_MODULE_VOLTAGE",
+  CommandedEvaporativePurge = "COMMANDED_EVAPORATIVE_PURGE",
+  TimeRunWithMilOn = "TIME_RUN_WITH_MIL_ON",
+  TimeSinceTroubleCodesCleared = "TIME_SINCE_TROUBLE_CODES_CLEARED",
+  DistanceTraveledWithMilOn = "DISTANCE_TRAVELED_WITH_MIL_ON",
+  WarmUpsSinceCodesCleared = "WARM_UPS_SINCE_CODES_CLEARED",
 }
 
 export interface Sensor {
