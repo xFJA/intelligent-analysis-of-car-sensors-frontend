48db7a86a34fd8b4ff40259827c9d88f01d002a0
xfja <franjimenezaguilera@gmail.com>
2020-05-30 07:24:34 +0200

    Add getDatasets method to api

---
+++ src/api/api.ts
@@ -0,0 +1,9 @@
+// TODO: Add the base url by config or env
+const BASE_URL: string = "http://localhost:8080/";
+
+// TODO: Add type to fetch
+export class Api {
+  getDatasets = async () => {
+    return await fetch(BASE_URL + "datasets");
+  };
+}
