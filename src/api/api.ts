cc845008f0e5bb52b6987064a89d5fdde268136f
xfja <franjimenezaguilera@gmail.com>
2020-05-30 16:46:46 +0200

    Add dataset upload flow

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 // TODO: Add the base url by config or env
 const BASE_URL: string = "http://localhost:8080/";
 
@@ -7,10 +9,13 @@ export class Api {
     return await fetch(BASE_URL + "datasets");
   };
 
-  addDataset = async () => {
-      return await fetch(BASE_URL + "datasets", {
-          method: "POST",
-          
-      })
-  }
+  addDataset = async (file: File) => {
+    let form = new FormData();
+    form.append("csv", file);
+
+    return await axios(BASE_URL + "datasets", {
+      method: "POST",
+      data: form,
+    });
+  };
 }
--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { DropzoneArea } from "material-ui-dropzone";
 import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import SendIcon from "@material-ui/icons/Send";
+import { Api } from "../api/api";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert, { AlertProps } from "@material-ui/lab/Alert";
+
+const service = new Api();
+
+// TODO: Create reusable component for feedback along all the app
+function Alert(props: AlertProps) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+const vertical = "bottom",
+  horizontal = "left";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      padding: 50,
+      padding: "100px 300px",
       backgroundColor: theme.palette.background.paper,
+    },
+    sendButton: {
+      marginTop: 25,
+    },
+    progress: {
+      marginTop: 25,
     },
   })
 );
@@ -14,17 +35,77 @@ const useStyles = makeStyles((theme: Theme) =>
 export const Upload: React.FC = (props) => {
   const classes = useStyles();
 
-  const onChange = () => {};
+  const [csvFile, setCSVFile] = useState<File>();
+  const [progress, setProgress] = useState<boolean>(false);
+  const [snackbarOpen, setSnackbarOpen] = useState<boolean>(false);
+  const [snackbarSeverity, setSnackbarSeverity] = useState<
+    AlertProps["severity"]
+  >();
+  const [snackbarText, setSnackbarText] = useState<string>("");
+
+  const onSend = () => {
+    if (!csvFile) return;
+    setProgress(true);
+    service
+      .addDataset(csvFile)
+      .then(() => {
+        setSnackbarSeverity("success");
+        setSnackbarText("The csv file was stored succesfully!");
+      })
+      .catch((e) => {
+        setSnackbarSeverity("error");
+        setSnackbarText(e.response.data.error);
+      })
+      .finally(() => {
+        setSnackbarOpen(true);
+        setProgress(false);
+      });
+  };
+
+  const handleSnackbarClose = (
+    event?: React.SyntheticEvent,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setSnackbarOpen(false);
+  };
 
   return (
     <div className={classes.root}>
       <DropzoneArea
-        onChange={onChange}
+        onChange={(files) => {
+          setCSVFile(files[0]);
+        }}
         acceptedFiles={[".csv, application/vnd.ms-excel, text/csv"]}
         filesLimit={1}
         dropzoneText={"Upload the CSV file"}
         showFileNames={true}
       />
+      {csvFile && (
+        <Button
+          className={classes.sendButton}
+          variant="contained"
+          color="primary"
+          endIcon={<SendIcon />}
+          onClick={onSend}
+        >
+          Send
+        </Button>
+      )}
+      {progress && <LinearProgress className={classes.progress} />}
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={6000}
+        onClose={handleSnackbarClose}
+        anchorOrigin={{ vertical, horizontal }}
+      >
+        <Alert onClose={handleSnackbarClose} severity={snackbarSeverity}>
+          {snackbarText}
+        </Alert>
+      </Snackbar>
     </div>
   );
 };
