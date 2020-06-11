a26b0ebb0b7af5d349203bcdd4f499c3d4015e3f
fran <franjimenezaguilera@gmail.com>
2020-06-11 21:39:08 +0200

    Add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Home } from "./components/Home";
 import { Upload } from "./components/Upload";
+import { Header } from "./components/Header";
 
 export const App: React.FC = () => {
+
+
   return (
     <Router>
+      <Header />
       <Switch>
         <Route exact path="/">
           <Home />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,21 +2,71 @@ import React from "react";
 import {
   AppBar,
   Toolbar,
-  IconButton,
   Typography,
   Button,
+  makeStyles,
+  Theme,
+  createStyles,
+  IconButton,
 } from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
+import { Link } from "react-router-dom";
+import HomeIcon from "@material-ui/icons/Home";
+import { useHistory } from "react-router-dom";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    link: {
+      textDecoration: "none",
+    },
+    linkGroup: {
+      marginLeft: 50,
+      marginRight: 20,
+      right: 0,
+      position: "absolute",
+    },
+    button: {
+      color: theme.palette.common.white,
+    },
+    homeButton: {
+      color: theme.palette.common.white,
+    },
+  })
+);
 
 export const Header: React.FC = (_) => {
+  const classes = useStyles();
+
+  let history = useHistory();
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <MenuIcon />
+        <IconButton
+          edge="start"
+          onClick={() => {
+            history.push("/");
+          }}
+        >
+          <HomeIcon className={classes.homeButton} />
         </IconButton>
-        <Typography variant="h6">News</Typography>
-        <Button color="inherit">Login</Button>
+        <Button
+          className={classes.button}
+          onClick={() => {
+            history.push("/");
+          }}
+        >
+          <Typography variant="h5">
+            Intelligent analysis of car's sensors
+          </Typography>
+        </Button>
+
+        <div className={classes.linkGroup}>
+          <Link className={classes.link} to="/upload">
+            <Button className={classes.button}>
+              <Typography variant="subtitle1">Upload</Typography>
+            </Button>
+          </Link>
+        </div>
       </Toolbar>
     </AppBar>
   );
